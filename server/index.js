f5eff45b4f02916cab240dc9ac3dfb68f2d29af9
Michael Lee <michaellee2245@gmail.com>
2018-11-03 10:10:22 -0600

    database added and connected

--- server/index.js
+++ server/index.js
@@ -3,10 +3,18 @@ require ('dotenv').config();
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const massive = require ('massive');
+const controller = require ('./controller.js')
 
 const app = express();
 
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive(CONNECTION_STRING)
+.then(dbInstance => {
+    app.set('db', dbInstance)
+    console.log('Database is connected')
+})
+.catch((error) => console.error('Database connection error', error))
 
 app.use(bodyParser.json());
 
