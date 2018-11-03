fe2aa6f15e92d6da17174ca66db851beb1692872
Michael Lee <michaellee2245@gmail.com>
2018-11-03 14:15:46 -0600

    add new user works

--- server/index.js
+++ server/index.js
@@ -3,7 +3,9 @@ require ('dotenv').config();
 const express = require ('express');
 const bodyParser = require ('body-parser');
 const massive = require ('massive');
-const controller = require ('./controller.js')
+const controller = require ('./controller.js');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors')
 
 const app = express();
 
@@ -16,8 +18,27 @@ massive(CONNECTION_STRING)
 })
 .catch((error) => console.error('Database connection error', error))
 
+app.use(cors());
 app.use(bodyParser.json());
 
+app.post('/new_user', (req, res) => {
+    const db = req.app.get('db')
+    const { username, password } = req.body
+
+    bcrypt.hash(password, null, null, (err, hash) => {
+        if (err){
+            return res.send('An error occurred while hashing.')
+        }
+        db.new_user([username, hash])
+        .then(() => {
+            res.status(200).send('User created successfully.')
+        })
+        .catch((error) => {
+            res.status(500).send(error)
+        })
+    })
+
+})
 
 app.listen(SERVER_PORT, () => {
     console.log(`Server connected and running on port ${SERVER_PORT}`);
--- src/component/Auth/Auth.js
+++ src/component/Auth/Auth.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react';
+import './auth.css';
+import axios from 'axios'
 
 class Auth extends Component {
 
-    render(){
-        return(
-            <div>Auth</div>
+    state = {
+        usernameInput: '',
+        passwordInput: ''
+    }
+
+    onInputChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+        // console.log('Username', this.state.usernameInput)
+        // console.log('Password', this.state.passwordInput)
+    }
+
+    newUser = () => {
+        axios.post('http://localhost:8080/new_user', {username: this.state.usernameInput, password: this.state.passwordInput})
+        .then(response => console.log (response))
+    }
+
+    render() {
+        return (
+            <div className="background-container">
+                <div className="main-auth-container">
+                    <div>Logo</div>
+                    <div className="auth-container-inputs">
+                        <label>Username:</label>
+                        <input name="usernameInput" value={this.state.usernameInput} onChange={this.onInputChange} />
+                        <label>Password:</label>
+                        <input name="passwordInput" value={this.state.passwordInput} onChange={this.onInputChange} />
+                    </div>
+                    <div>
+                        <button>Login</button>
+                        <button onClick={this.newUser} >Register</button>
+                    </div>
+                </div>
+            </div>
         )
     }
 }
