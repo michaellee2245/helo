37b312a941fd59671322f5f4245517d789ff7690
Michael Lee <michaellee2245@gmail.com>
2018-11-03 13:12:51 -0600

    nav bar added

--- src/component/Nav/Nav.js
+++ src/component/Nav/Nav.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './nav.css';
 
 function Nav(props) {
     if (props.location.pathname !== '/') {
         return (
-            <div>
-                Nav
+            <div className="main-nav-container">
+                <div className="nav-top-items">
+                    <Link className="home" to='/dashboard'></Link>
+                    <Link className="new-post" to='/post'></Link>
+                </div>
+                <div className="nav-bottom-items">
+                    <Link className ="logout" to='/'></Link>
+                </div>
             </div>
+
         )
     }
     return null
